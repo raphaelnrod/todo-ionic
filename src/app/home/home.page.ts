1b2e107d4c3f09d18fcc624ad6ee07811c047345
Raphael Augusto <raphaelnrod@gmail.com>
2020-08-01 00:28:56 -0300

    some updates

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,50 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private alertCtrl : AlertController) {}
 
+  async addTask(){
+    const alert = await this.alertCtrl.create({
+      header: 'Adicionar nova tarefa',
+      cssClass: 'custom-alertDanger',
+      inputs: [
+        {
+          name: 'task',
+          type: 'text',
+          cssClass: 'myalert',
+          placeholder: 'O que deseja fazer?'
+        }
+      ],
+      buttons: [
+        { 
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'myalert',
+          handler: () => {
+            console.log("cancel funcionando");
+          }
+        },
+        { 
+          text: 'Adicionar',
+          cssClass: 'theme-color',
+          handler: (form) => {
+            this.add(form.task);
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  delete() {
+    console.log("Apagando item")
+  }
+
+  update() {
+    console.log("Atualizando item")
+  }
+
+  unread(item){
+    console.log("Unread funcionando")
+  }
 }
